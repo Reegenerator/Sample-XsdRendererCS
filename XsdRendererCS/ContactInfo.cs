58eb16b1bc717bb1329360af0c8a91a28f3e5467
Tedy Pranolo <tedy.pranolo@gmail.com>
2014-02-04 15:53:12 +0800

    Adding excluded ContactInfo.cs

---
+++ XsdRendererCS/ContactInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XsdRendererCS
+{
+    partial class Person
+    {
+        public string CombinedAddress
+        {
+            get { return String.Format("{0} {1}. {2} {3}. {4}",Address.Street, Address.Number, Address.City, Address.State , Address.Country) ; }
+        }
+    }
+}
